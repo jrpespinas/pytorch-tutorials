9bcfa3a84bc0274cfa0cc7d87d7b31a1c8a355be
jrpespinas <jrpespinas@gmail.com>
2020-11-21 17:06:46 +0800

    feat: add forward function

--- neural_networks/neural_networks.py
+++ neural_networks/neural_networks.py
@@ -11,3 +11,12 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16 * 6 * 6, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
