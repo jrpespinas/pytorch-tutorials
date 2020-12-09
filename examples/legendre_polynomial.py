e374d63668a0258ff32b198a4f56396e3eeb924c
jrpespinas <jrpespinas@gmail.com>
2020-12-09 13:42:35 +0800

    chore: add legendre polynomial example

---
+++ examples/legendre_polynomial.py
@@ -0,0 +1,25 @@
+import math
+import torch
+
+
+class LegendrePolynomial3(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        """
+        In the forward pass we receive a Tensor containing the input and return 
+        a Tensor containing the output. ctx is a context object that can be used
+        to stash information for backward computation. You can cache arbitrary
+        objects for use in the backward pass using the ctx.save_for_backward method.
+        """
+        ctx.save_for_backward(input)
+        return 0.5 * (5 * input ** 3 - 3 * input)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        """
+        In the backward pass we receive a Tensor containing the gradientof the loss
+        with respect to the output, and we need to compute the gradient of the loss
+        with respect to the input.
+        """
+        input, = ctx.saved_tensors
+        return grad_output * 1.5 * (5 * input ** 2 - 1)
